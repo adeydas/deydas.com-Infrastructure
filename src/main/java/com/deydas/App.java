b315a7211b108c07f470bbc06c479009dd936489
Abhishek Dey Das <abhishek@abhis.ws>
2019-11-24 09:25:21 +0530

    Initial commit

---
+++ src/main/java/com/deydas/App.java
@@ -0,0 +1,13 @@
+package com.deydas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
